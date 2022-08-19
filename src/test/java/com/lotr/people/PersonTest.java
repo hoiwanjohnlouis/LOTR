bce3785d08f69be8add6b4adc7b27f052c15a21e
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2022-08-19 12:07:02 -0400

    (20220818-172100) 1. Migrated source code to G:\myGit\github from BOX\my\github.

--- src/test/java/com/lotr/people/PersonTest.java
+++ src/test/java/com/lotr/people/PersonTest.java
@@ -16,8 +16,8 @@
 
 package com.lotr.people;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
